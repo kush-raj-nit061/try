3e05e5cd8d145cccf3c70e4dd7fd5700309cc557
kush-raj-nit061 <96692422+kush-raj-nit061@users.noreply.github.com>
2022-03-16 15:11:13 +0530

    Add files via upload

---
+++ womens.c
@@ -0,0 +1,245 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+int i,j,k,l,m;
+system("color 0c");
+printf("\033[1;33m");
+for(i=0;i<=11;i++)
+{
+for(j=0;j<=2;j++)
+{
+printf("\14");
+}
+for(k=10;k>=i;k--)
+{
+printf(" ");
+}
+for(j=0;j<=2;j++)
+{
+printf("\14");
+}
+for(m=1;m<=i;m++)
+{
+printf("  ");
+}
+for(j=0;j<=2;j++)
+{
+printf("\14");
+}
+for(l=10;l>=i;l--)
+{
+printf(" ");
+}
+for(j=0;j<=2;j++)
+{
+printf("\14");
+}
+printf("\n");
+}
+
+  printf("\n");
+
+
+printf("\033[1;35m");
+
+
+
+    for(i = 1;i<=2;i++){
+    for(j= 1 ;j<34;j++){
+        printf("\3");
+    }
+    printf("\n");
+}
+for(i = 1;i<=6 ;i++){
+    for(j= 1 ;j<34;j++){
+            if(j<=3 || j>=31){
+        printf("\3");
+    }
+    else{
+        printf(" ");
+    }
+    }
+    printf("\n");
+}
+for(i = 1;i<=2;i++){
+    for(j= 1 ;j<34;j++){
+        printf("\3");
+    }
+    printf("\n");
+}
+
+printf("\n");
+
+
+
+printf("\033[1;36m");
+
+
+
+for(i=0;i<=11;i++)
+ {
+  for(j=0;j<=2;j++)
+  {
+   printf("\13");
+  }
+  for(k=1;k<=i;k++)
+    {
+    printf(" ");
+ }
+ for(j=0;j<=2;j++)
+  {
+   printf("\13");
+  }
+  for(m=10;m>=i;m--)
+  {
+   printf("  ");
+  }
+  for(j=0;j<=2;j++)
+  {
+   printf("\13");
+  }
+  for(k=1;k<=i;k++)
+  {
+   printf(" ");
+  }
+  for(j=0;j<=2;j++)
+  {
+   printf("\13");
+  }
+
+ printf("\n");
+ }
+
+ printf("\n");
+
+
+
+
+ printf("\033[1;32m");
+
+
+
+     for(i = 1;i<=2;i++){
+    for(j= 1 ;j<34;j++){
+        printf("\2");
+    }
+    printf("\n");
+}
+for(i = 1;i<=3 ;i++){
+    for(j= 1 ;j<=4;j++){
+        printf("\2");
+    }
+    printf("\n");
+}
+ for(i = 1;i<=2;i++){
+    for(j= 1 ;j<34;j++){
+        printf("\2");
+    }
+    printf("\n");
+ }
+ for(i = 1;i<=3 ;i++){
+    for(j= 1 ;j<=4;j++){
+        printf("\2");
+    }
+    printf("\n");
+}
+for(i = 1;i<=2;i++){
+    for(j= 1 ;j<34;j++){
+        printf("\2");
+    }
+    printf("\n");
+}
+
+
+printf("\033[1;31m");
+
+
+
+    printf("\n");
+
+
+
+for(i=0;i<=11;i++)
+ {
+  for(j=0;j<=2;j++)
+  {
+   printf("\17");
+   printf(" ");
+  }
+  for(k=1;k<=i;k++)
+    {
+    printf(" ");
+ }
+ for(j=0;j<=2;j++)
+  {
+   printf(" ");
+   printf("\17");
+   printf(" ");
+  }
+     for(l=10;l>=i;l--)
+     {
+      printf(" ");
+  }
+  for(j=0;j<=2;j++)
+  {
+   printf("\17");
+   printf(" ");
+  }
+
+ printf("\n");
+ }
+
+ printf("\n");
+
+
+
+ printf("\033[1;37m");
+
+
+
+
+     for(i = 1;i<=2;i++){
+    for(j= 1 ;j<34;j++){
+        printf("\25");
+    }
+    printf("\n");
+}
+for(i = 1;i<=3 ;i++){
+    for(j= 1 ;j<4;j++){
+        printf("\25");
+    }
+    printf("\n");
+}
+ for(i = 1;i<=2;i++){
+    for(j= 1 ;j<34;j++){
+        printf("\25");
+    }
+    printf("\n");
+ }
+ for(i = 1;i<=3 ;i++){
+    for(j= 1 ;j<31;j++){
+        printf(" ");
+    }
+    if(j>=31){
+    for(j=31;j<34;j++){
+        printf("\25");
+    }
+    }
+    printf("\n");
+}
+for(i = 1;i<=2;i++){
+    for(j= 1 ;j<34;j++){
+        printf("\25");
+    }
+    printf("\n");
+}
+
+
+
+
+getch();
+}
+
